7280ff82e7e8e27a9949442cb9f6f5c964650b5b
sas1300 <sascode13@gmail.com>
2020-10-04 21:34:26 -0500

    save button function

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,27 @@ var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
 
 
+// I DON'T THINK I NEED THIS 
+//create a new task in text area
+//var createTask = function(taskText) {
+   //var taskEnter = $("<textarea>"); 
+//edit an existing task in text area
+//remove an existing task in text area
+
+
+//save content of text area into local storage when save button is clicked
+//event listener...click event function
+
+//save button click using jQuery
+$("saveButton").on("click", function() {
 
 
 
+})
 
+
+
+//determine if time slot of task area 
+    //  has passed - color grey
+    // current hour - color red
+    // upcoming hour - color green
